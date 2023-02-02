990434c719d576365bcae3215b73d53fbaa502db
jung94min <jung81min@gmail.com>
2023-02-02 21:23:34 +0900

    SREV-39 WPF 공부 1. 데이터베이스 연결   1) ListView를 이용해 테이블 데이터 출력

---
+++ Practice_Kwon/DbTableBinding/DbTableBinding/EmpViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbTableBinding
+{
+  internal class EmpViewModel
+  {
+    int empno = 0;
+    string ename = string.Empty;
+    string job = string.Empty;
+
+    // public 프로퍼티
+    public int Empno
+    {
+      get { return empno; }
+      set { this.empno = value; }
+    }
+
+    // public 프로퍼티
+    public string Ename
+    {
+      get { return ename; }
+      set { this.ename = value; }
+    }
+
+    // public 프로퍼티
+    public string Job
+    {
+      get { return job; }
+      set { this.job = value; }
+    }
+  }
+}
---
+++ Practice_Kwon/DbTableBinding/DbTableBinding/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using Oracle.DataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows;
+
+
+namespace DbTableBinding
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    OracleConnection conn;
+
+    private void DB_Connect(object sender, RoutedEventArgs e)
+    {
+      try
+      {
+        string strCon = "data source=xe;User ID=scott;Password=1234";
+        //      string strCon = @"XE =
+        //(DESCRIPTION =
+        //  (ADDRESS = (PROTOCOL = TCP)(HOST = DESKTOP-VST2LD4)(PORT = 1521))
+        //  (CONNECT_DATA =
+        //    (SERVER = DEDICATED)
+        //    (SERVICE_NAME = XE)
+        //  )
+        //);User Id=scott;Password=1234";
+        conn = new OracleConnection(strCon);
+        conn.Open();
+
+        MessageBox.Show("DB Connection OK!");
+      }
+      catch (Exception error)
+      {
+        MessageBox.Show(error.ToString());
+      }
+    }
+
+    private void Select_Emp(object sender, RoutedEventArgs e)
+    {
+      string sql = "select empno, ename, job from emp";
+
+      OracleCommand comm = new OracleCommand();
+      if (conn == null) DB_Connect(this, null);
+      comm.Connection = conn;
+      comm.CommandText = sql;
+
+      OracleDataReader reader = comm.ExecuteReader(CommandBehavior.CloseConnection);
+      List<EmpViewModel> emps = new List<EmpViewModel>();
+      while (reader.Read())
+      {
+        emps.Add(new EmpViewModel()
+        {
+          Empno = reader.GetInt32(reader.GetOrdinal("empno")),
+          Ename = reader.GetString(reader.GetOrdinal("ename")),
+          Job = reader.GetString(reader.GetOrdinal("job"))
+          // Empno = reader.GetInt32(0),
+          // Ename = reader.GetString(1),
+          // Job = reader.GetString(2)
+        });
+      }
+      lstView.ItemsSource = emps;
+      reader.Close();
+    }
+  }
+}
